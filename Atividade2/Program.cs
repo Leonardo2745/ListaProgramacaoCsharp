ce6fb56c784aac4558ec67eaa1a03a10554ecf43
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-12-03 15:17:22 -0300

    Atividade 2 feita

---
+++ Atividade2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um número inteiro:");
+String entrada = Console.ReadLine();
+int num = int.Parse(entrada);
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("O número " + num + " é par!");
+}
+else
+{
+    Console.WriteLine("O número " + num + " é ímpar!");
+}
