f877d5cb4669e788d8b15af7da53b00c13768e27
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-12-03 17:18:30 -0300

    Atividade 6 feita

---
+++ Atividade6/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite 5 números:" + "\n");
+int[] vetor = new int[10];
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write("Digite o " + (i + 1) + "º número: ");
+    string entrada = Console.ReadLine();
+    int num = int.Parse(entrada);
+    vetor[i] = num;
+
+}
+
+Console.WriteLine("\nOs valores inseridos de trás pra frente são: ");
+
+for(int i = vetor.Length - 1; i >= 0; i--)
+{
+    Console.Write(vetor[i] + " ");
+}
+
+
+
