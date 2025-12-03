62162b7090f4c0a5e3cb8c5fa56be88506f02bd8
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-12-03 16:14:03 -0300

    Atividade 4 feita

---
+++ Atividade4/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um número inteiro para saber o seu fatorial:");
+
+string entrada = Console.ReadLine();
+int num = int.Parse(entrada);
+
+
+int Fatorial(int x)
+{
+    int fatorial = 1;
+
+    for (int i = 1; i <= x; i++)
+    {
+        fatorial *= i;
+    }
+
+    return fatorial;
+}
+
+
+
+Console.WriteLine("O fatorial do número " + num + " é : " + Fatorial(num));
