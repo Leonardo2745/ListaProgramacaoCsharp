ea3c21561fe5e91830ab9983fe933a636cb5adc9
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-12-03 16:53:36 -0300

    Atividade 5 feita

---
+++ Atividade5/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite quantos números da sequência de Fibonacci serão mostrados: ");
+int n = 0, primeiro = 0, segundo = 1, proximo = 0;
+
+string entrada = Console.ReadLine();
+n = int.Parse(entrada);
+
+Console.WriteLine("Os " + n + " primeiros números da sequência de Fibonacci são:");
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(primeiro + " ");
+    
+    proximo = primeiro + segundo;
+    primeiro = segundo;
+    segundo = proximo;
+}
