f00876b5c8ff67f731d3e70d59f64dc11ff04f6a
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-12-03 18:22:05 -0300

    Atividade 7 feita

---
+++ Atividade7/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite 10 números inteiros");
+int[] vetor = new int[10];
+int soma = 0, media = 0, numMaiorMed = 0;
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.Write("Digite o " + (i + 1) + "º número: ");
+    string entrada = Console.ReadLine();
+    int num = int.Parse(entrada);
+    vetor[i] = num;
+
+    soma += num;
+
+}
+
+media = (soma / vetor.Length);
+
+Console.WriteLine("A média dos números digitados é: " + media);
+
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    if (vetor[i] > media)
+    {
+        numMaiorMed += 1;
+    }
+}
+
+Console.Write("Quantidade de números maiores que a média: " + numMaiorMed);
