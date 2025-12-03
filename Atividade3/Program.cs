cc8f339b784b13fc591628c99bbf036ab3b8011e
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-12-03 15:29:36 -0300

    Atividade 3 feita

---
+++ Atividade3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insira a temperatura para ser convertida (ºC):");
+string entrada = Console.ReadLine();
+double c = double.Parse(entrada);
+double f = 0;
+
+f = (c * 1.8 + 32);
+
+Console.WriteLine("A temperatura de " + c + "ºC convertida para Fahrenheit é : " + f + "ºF");
+
