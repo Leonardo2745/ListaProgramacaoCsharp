ba669f66e9175ee252564c0b5d4daf9566cdba2d
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2025-12-03 15:24:28 -0300

    Correção de nomenclatura da atividade 2

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Digite um número inteiro:");
-String entrada = Console.ReadLine();
+string entrada = Console.ReadLine();
 int num = int.Parse(entrada);
 
 if (num % 2 == 0)
